0687105305790053e894a7f2d0c70fbb5579a152
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-05-29 21:42:57 -0600

    learning how to create a database
    Implementing a sales database through udemy

---
+++ MySQL/sql_scripts/1.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS Sales;
+ -- or we could use
+ -- schema and database go hand in hand
+-- CREATE SCHEMA IF NOT EXISTS Sales;
+
+USE Sales;
+-- helps tell MySql the Database to use
