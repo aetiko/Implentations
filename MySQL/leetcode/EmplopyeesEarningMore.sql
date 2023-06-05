c0be3e916c3fbce9d3b007af32db3305293144c8
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-06-05 13:06:22 -0600

    combine two tables using left join

---
+++ MySQL/leetcode/EmplopyeesEarningMore.sql
@@ -0,0 +1,8 @@
+Create database if not exists Leetcode;
+use Leetcode;
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table Employee;
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None');
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None');
