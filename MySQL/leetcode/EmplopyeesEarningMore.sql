dfaa4ec903270f199c4689a5baa60a593292983e
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-06-05 13:30:52 -0600

    added 2 scripts

--- MySQL/leetcode/EmplopyeesEarningMore.sql
+++ MySQL/leetcode/EmplopyeesEarningMore.sql
@@ -6,3 +6,9 @@ insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', 
 insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
 insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None');
 insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None');
+
+-- Write an SQL query to find the employees who earn more than their managers.
+
+select t1.name as Employee from Employee t1 join Employee t2 
+on t1.managerId = t2.id
+where t1.salary > t2.salary;
---
+++ MySQL/leetcode/duplicateEmails.sql
@@ -0,0 +1,10 @@
+Create table If Not Exists Person (id int, email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'a@b.com');
+insert into Person (id, email) values ('2', 'c@d.com');
+insert into Person (id, email) values ('3', 'a@b.com');
+
+-- Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+select email from Person
+group by email having count(email)>1;
