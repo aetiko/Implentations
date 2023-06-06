691d1e9cd9c86e098f44b2c0180012e93b9e5a4a
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-06-05 22:08:53 -0600

    update 1.sql

--- MySQL/sql_scripts/1.sql
+++ MySQL/sql_scripts/1.sql
@@ -5,3 +5,17 @@ CREATE DATABASE IF NOT EXISTS Sales;
 
 USE Sales;
 -- helps tell MySql the Database to use
+
+create table if not exists sales ( 
+purchase_number int not null primary key auto_increment, 
+date_of_purchase date not null, 
+customer_id int, 
+item_code varchar(10)
+);
+
+create table if not exists customers (
+customer_id int not null primary key,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+email_address varchar(255),
+number_of_complaints int);
