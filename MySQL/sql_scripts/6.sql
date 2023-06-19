aaade32a7b3db6ec093c231f1d86c4e8605ab74b
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-06-19 08:41:44 -0600

    using the delete function

--- MySQL/sql_scripts/6.sql
+++ MySQL/sql_scripts/6.sql
@@ -1,6 +1,9 @@
 -- update statement
 -- To allow for update check preferences > SQL editor > unclick safe updates
 
+commit;
+rollback;
+
 SELECT 
     *
 FROM
@@ -13,3 +16,20 @@ SET
     first_name = 'Ayoola'
 WHERE
     emp_no = 999904;
+    
+-- learning to use the delete statement
+
+SELECT 
+    *
+FROM
+    titles
+WHERE
+    emp_no = 999903;
+    
+DELETE FROM employees 
+WHERE
+    emp_no = 999903;
+    
+select * from departments;
+delete from departments where dept_no = 'd010';
+
