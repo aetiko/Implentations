befa5a1e1783395ab38d60732306a2b277d5a0bd
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-07-10 21:49:00 -0600

    update 8.sql
    - self join
    - nesting in SQL

--- MySQL/sql_scripts/8.sql
+++ MySQL/sql_scripts/8.sql
@@ -33,21 +33,170 @@ WHERE
             titles t
         WHERE
             t.emp_no = e.emp_no and title = 'Assistant Engineer' );
+            
+-- subquieries nested in SELECT AND FROM
+
 SELECT 
-    e.emp_no AS employee_ID,
-    MIN(de.dept_no) AS department_code,
-    (SELECT 
-            emp_no
-        FROM
-            dept_manager
-        WHERE
-            emp_no = 110022) AS manager_ID
+    a.*
 FROM
-    employees e
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS a 
+UNION SELECT 
+    b.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS b;
+    
+    
+drop table if exists emp_manager;
+CREATE TABLE IF NOT EXISTS emp_manager (
+    emp_no INT(11) NOT NULL,
+    dept_no CHAR(4),
+    manager_no INT(11) NOT NULL
+);
+-- Your query skeleton must be:
+-- Insert INTO emp_manager SELECT U.* FROM
+
+                 -- (A)
+
+-- UNION (B) UNION (C) UNION (D) AS U;
+-- A and B should be the same subsets used in the last lecture (SQL Subqueries Nested in SELECT and FROM). 
+-- In other words, assign employee number 110022 as a manager to all employees from 10001 to 10020 (this must be subset A), 
+-- and employee number 110039 as a manager to all employees from 10021 to 10040 (this must be subset B).
+-- Use the structure of subset A to create subset C, where you must assign employee number 110039 as a manager to employee 110022.
+-- Following the same logic, create subset D. Here you must do the opposite - assign employee 110022 as a manager to employee 110039. 
+
+insert into emp_manager select U.* 
+from 
+(SELECT 
+    a.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS a 
+UNION SELECT 
+    b.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110022
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS b 
+    UNION SELECT 
+    c.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110039
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS c
+    UNION SELECT 
+    d.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS d) as U; 
+    
+-- Self join
+
+SELECT 
+    *
+FROM
+    emp_manager
+ORDER BY emp_manager.emp_no;
+
+SELECT 
+    e1.*
+FROM
+    emp_manager e1
         JOIN
-    dept_emp de ON e.emp_no = de.emp_no
+    emp_manager e2 ON e1.emp_no = e2.manager_no
 WHERE
-    e.emp_no <= 10020
-GROUP BY e.emp_no
-ORDER BY e.emp_no;
+    e2.emp_no IN (SELECT 
+            manager_no
+        FROM
+            emp_manager);
+
+
+
+
+
 
