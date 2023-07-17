b3c788a7a78a006dea2d0a8b046d90e4984483b2
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2023-07-17 10:07:43 -0600

    update 9.sql
    Worked on stored procedures
    - without inputs
    - 1 or more inputs

--- MySQL/sql_scripts/9.sql
+++ MySQL/sql_scripts/9.sql
@@ -57,3 +57,41 @@ delimiter ;
 
 call employees.average_employees_salary();
 
+drop procedure select_employees;
+
+delimiter $$
+create procedure emp_salary(in p_emp_no integer)
+begin  
+	select e.first_name, e.last_name, avg(s.salary)
+    from employees e 
+    join salaries s 
+    on e.emp_no = s.emp_no 
+    where e.emp_no = p_emp_no;
+end$$
+delimiter ;
+
+drop procedure emp_salary;
+
+call employees.emp_salary(10001);
+
+delimiter $$
+create procedure emp_avg_salary_out(in p_emp_no integer, out p_avg_salary decimal(10,2))
+begin  
+	select avg(s.salary) into p_avg_salary from employees e join salaries s on e.emp_no = s.emp_no where e.emp_no = p_emp_no;
+end$$
+delimiter ;
+
+drop procedure emp_avg_salary_out;
+
+call emp_avg_salary_out(10001);
+
+delimiter $$
+create procedure emp_info(in p_first_name varchar(11), in p_last_name varchar(11), out p_emp_no integer)
+begin  
+	select e.emp_no into p_emp_no from employees e where e.first_name = p_first_name and e.last_name = p_last_name;
+end$$
+delimiter ;
+
+drop procedure emp_info;
+
+call emp_info('Ayoola', 'Etiko');
