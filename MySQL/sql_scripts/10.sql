ec1759ee7b8f995effd26b4a1fdf1883c9ff5881
ayo <nurudeenetiko12@gmail.com>
2024-01-18 01:58:13 -0700

    Update 10.sql
    Worked on 3 various variables mainly, global, locAL AND SESSION PERTAINING TO LEARNING ABOUT THAT SCOPE AND ACCESS IN DATABASE.

--- MySQL/sql_scripts/10.sql
+++ MySQL/sql_scripts/10.sql
@@ -1,3 +1,28 @@
 -- session variables--  
 set @s_var1 = 3;
 select @s_var1;
+
+-- Triggers --
+Use employees;
+commit;
+DELIMITER $$
+
+CREATE TRIGGER before_salaries_insert
+BEFORE INSERT ON salaries
+FOR EACH ROW
+BEGIN 
+	IF NEW.salary < 0 THEN 
+		SET NEW.salary = 0; 
+	END IF; 
+END $$
+
+DELIMITER ;
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no = '10001';
+
+INSERT INTO salaries VALUES ('10001', -92891, '2010-06-22', '9999-01-01');
