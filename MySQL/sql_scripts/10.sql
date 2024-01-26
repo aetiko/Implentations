665034d42fbe4db43f348745e4099f35609a16dc
ayo <nurudeenetiko12@gmail.com>
2024-01-26 13:03:58 -0700

    update 10.sql
    More on triggers

--- MySQL/sql_scripts/10.sql
+++ MySQL/sql_scripts/10.sql
@@ -56,3 +56,100 @@ FROM
 WHERE
     emp_no = '10001'
         AND from_date = '2010-06-22';
+        
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+UPDATE salaries 
+SET 
+    salary = - 50000
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no = '10001'
+        AND from_date = '2010-06-22';
+        
+SELECT SYSDATE();
+
+SELECT DATE_FORMAT(SYSDATE(), '%y-%m-%d') AS today As an exercise, try to understand the following query. Technically, it regards the creation of a more complex trigger. 
+#It is of the size that professionals often have to deal with.
+
+DELIMITER $$
+
+CREATE TRIGGER trig_ins_dept_mng
+AFTER INSERT ON dept_manager
+FOR EACH ROW
+BEGIN
+	DECLARE v_curr_salary int;
+    
+    SELECT 
+		MAX(salary)
+	INTO v_curr_salary FROM
+		salaries
+	WHERE
+		emp_no = NEW.emp_no;
+
+	IF v_curr_salary IS NOT NULL THEN
+		UPDATE salaries 
+		SET 
+			to_date = SYSDATE()
+		WHERE
+			emp_no = NEW.emp_no and to_date = NEW.to_date;
+
+		INSERT INTO salaries 
+			VALUES (NEW.emp_no, v_curr_salary + 20000, NEW.from_date, NEW.to_date);
+    END IF;
+END $$
+
+DELIMITER ;
+
+# After you are sure you have understood how this query works, please execute it and then run the following INSERT statement.  
+INSERT INTO dept_manager VALUES ('111534', 'd009', date_format(sysdate(), '%y-%m-%d'), '9999-01-01');
+
+# SELECT the record of employee number 111534 in the ‘dept_manager’ table, and then in the ‘salaries’ table to see how the output was affected. 
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no = 111534;
+    
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no = 111534;
+
+# Conceptually, this was an ‘after’ trigger that automatically added $20,000 to the salary of the employee who was just promoted as a manager. 
+# Moreover, it set the start date of her new contract to be the day on which you executed the insert statement.
+
+select * from employees;
+DELIMITER $$
+CREATE TRIGGER check_hire_date
+before insert on employees
+for each row
+begin
+if new.hire_date > sysdate() then
+set new.hire_date = date_format(sysdate(), '%y-%m-%d');
+end if;
+end $$
+delimiter ;
+
+
+INSERT employees VALUES ('999905', '1970-01-31', 'John', 'Johnson', 'M', '2025-01-01'); 
+
+# Finally, to restore the data in the database to the state from the beginning of this lecture, execute the following ROLLBACK statement. 
+ROLLBACK;
+
